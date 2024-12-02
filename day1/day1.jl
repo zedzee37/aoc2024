fa37568ebea32a9b679d173878161fc121265984
zedzee <carsonuderitz@gmail.com>
2024-12-02 18:34:53 -0500

    start of day1

---
+++ day1/day1.jl
@@ -0,0 +1,46 @@
+function parseLine(line::String)
+    current = 1
+
+    res = parseNum(line, current)
+    firstNum = res[1]
+    current = res[2]
+
+    current = consumeNonDigits(line, current)
+
+    res = parseNum(line, current)
+    secondNum = res[1]
+
+    return (firstNum, secondNum)
+end
+
+function parseNum(line::String, start::Int)
+    current = start
+
+    while (current <= lastindex(line) && isdigit(line[current]))
+        current += 1
+    end
+
+    numStr = line[start:current - 1]
+    return (parse(Int, numStr), current)
+end
+
+function consumeNonDigits(line::String, start::Int)
+    current = start
+
+    while (!isdigit(line[current]))
+        current += 1
+    end
+
+    return current
+end
+
+list1 = []
+list2 = []
+
+for line in readlines("input.txt")
+    res = parseLine(line)
+    num1 = res[1]
+    num2 = res[2]
+    push!(list1, num1)
+    push!(list2, num2)
+end
