1bc4a1738f57a9020dec8d9560ab63406bece8a8
zedzee <carsonuderits@gmail.com>
2024-12-03 01:54:18 -0500

    refactor

--- day2/day2.js
+++ day2/day2.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 
-function isDigit(char) {
-    return !isNaN(parseInt(char, 10)) && char !== ' ';
-}
-
 let rows = fs.readFile("input.txt", "utf-8", (err, text) => {
     if (err) {
         console.error("Error reading file:", err);
@@ -16,11 +12,3 @@ let rows = fs.readFile("input.txt", "utf-8", (err, text) => {
     lines.forEach(line => rows.push(line.split(" ").forEach(parseInt)));
     return rows;
 });
-
-function consumeNonDigits(line, start) {
-    let current = start;
-    while (current < line.length && !isDigit(line[current])) {
-        current++;
-    }
-    return current;
-}
