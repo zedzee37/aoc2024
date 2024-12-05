6dc6fba3298c36d3e60f5fa6a958bbcfbbcebd1e
zedzee <carsonuderitz@gmail.com>
2024-12-05 15:17:14 -0500

    day 4 finished

--- day4/main.go
+++ day4/main.go
@@ -64,16 +64,77 @@ func accumulateMatches(grid []string) uint {
 	return count
 }
 
-func main() {
-	data, err := os.ReadFile("input.txt")
-	check(err)
-
-	text := string(data)
-	fmt.Println(text)
-
+func part1(text string) {
 	grid := strings.Split(text, "\n")
 	grid = grid[0 : len(grid)-1]
 
 	matchCount := accumulateMatches(grid)
 	fmt.Println(matchCount)
+}
+
+const MAS_STRING = "MAS"
+
+func isOnEdge(grid []string, x int, y int) bool {
+	l := len(grid)
+	return x > 0 && y > 0 && x < l-1 && y < l-1
+}
+
+func hasX(grid []string, x int, y int) bool {
+	if grid[x][y] != MAS_STRING[1] || !isOnEdge(grid, x, y) {
+		return false
+	}
+
+	for dy := -1; dy <= 1; dy += 2 {
+		for dx := -1; dx <= 1; dx += 2 {
+			newX := x + dx
+			newY := y + dy
+
+			ch := grid[newX][newY]
+
+			if ch == MAS_STRING[0] || ch == MAS_STRING[2] {
+				toMatch := MAS_STRING[0]
+
+				if ch == MAS_STRING[0] {
+					toMatch = MAS_STRING[2]
+				}
+
+				newX = x - dx
+				newY = y - dy
+
+				if grid[newX][newY] != toMatch {
+					return false
+				}
+			} else {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func part2(text string) {
+	grid := strings.Split(text, "\n")
+	grid = grid[0 : len(grid)-1]
+
+	count := 0
+
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid); x++ {
+			if hasX(grid, x, y) {
+				count++
+			}
+		}
+	}
+
+	fmt.Println(count)
+}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	check(err)
+
+	text := string(data)
+
+	part2(text)
 }
