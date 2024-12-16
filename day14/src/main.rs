7bf660928986e1556ecce98f09015b31142fd610
zedzee <carsonuderitz@gmail.com>
2024-12-15 22:16:06 -0500

    day 14 part 1

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -7,6 +7,13 @@ struct Robot {
     v_y: i32,
 }
 
+impl Robot {
+    fn tick(&mut self, iterations: i32, width: i32, height: i32) {
+        self.x = (self.x + self.v_x * iterations).rem_euclid(width);
+        self.y = (self.y + self.v_y * iterations).rem_euclid(height);
+    }
+}
+
 fn parse_input(input: String) -> Vec<Robot> {
     let mut robots = Vec::<Robot>::new();
     let num_regex = Regex::new(r"(-?\d)+").unwrap();
@@ -15,9 +22,13 @@ fn parse_input(input: String) -> Vec<Robot> {
     for line in lines {
         let numbers: Vec<i32> = num_regex
             .find_iter(line.as_bytes())
-            .map(|str| 
-                std::str::from_utf8(str.as_bytes()).unwrap().parse::<i32>().unwrap()
-            ).collect();
+            .map(|str| {
+                std::str::from_utf8(str.as_bytes())
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap()
+            })
+            .collect();
 
         if numbers.len() == 4 {
             let robot = Robot {
@@ -30,10 +41,42 @@ fn parse_input(input: String) -> Vec<Robot> {
         }
     }
 
-    robots
+    return robots;
+}
+
+fn get_quadrant(x: i32, y: i32, width: i32, height: i32) -> i32 {
+    let center_x = width / 2;
+    let center_y = height / 2;
+
+    return if x < center_x && y < center_y {
+        1
+    } else if x > center_x && y < center_y {
+        2
+    } else if x < center_x && y > center_y {
+        3
+    } else if x > center_x && y > center_y {
+        4
+    } else {
+        0
+    };
+}
+fn simulate_robots(robots: &mut Vec<Robot>, width: i32, height: i32) -> i32 {
+    let mut robot_counts: [i32; 4] = [0; 4];
+    robots.iter_mut().for_each(|robot| {
+        robot.tick(100, width, height);
+        let quadrant = get_quadrant(robot.x, robot.y, width, height);
+
+        if quadrant != 0 {
+            robot_counts[(quadrant - 1) as usize] += 1;
+        }
+    });
+
+    return robot_counts.iter().product();
 }
 
 fn main() {
     let file_contents = std::fs::read_to_string("input.txt").unwrap();
-    let robots = parse_input(file_contents);
+    let mut robots = parse_input(file_contents);
+    let result = simulate_robots(&mut robots, 101, 103);
+    println!("{}", result)
 }
