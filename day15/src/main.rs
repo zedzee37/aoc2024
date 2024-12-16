c9582efc0a486a1fd451c5f232b97aee45b09be2
zedzee <carsonuderitz@gmail.com>
2024-12-16 08:58:04 -0500

    day 15 part 1

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,5 +1,4 @@
-use core::error;
-use std::{fmt::Error, fs, task::Context};
+use std::fs;
 
 #[derive(Debug)]
 enum Movement {
@@ -48,14 +47,75 @@ fn find_robot(grid: &Vec<Vec<char>>) -> Option<(usize, usize)> {
     return None;
 }
 
-fn simulate_robot(grid: &mut Vec<Vec<char>>, movements: &Vec<Movement>) -> Result<usize, Error> {
+fn is_off_grid(grid: &Vec<Vec<char>>, pos: (i32, i32)) -> bool {
+    return pos.0 < 0 || pos.1 < 0 || pos.1 >= grid.len() as i32 || pos.0 >= grid[pos.1 as usize].len() as i32;
+}
+
+fn simulate_robot(grid: &mut Vec<Vec<char>>, movements: &Vec<Movement>) -> usize {
     let mut pos = find_robot(grid).unwrap();
 
     for movement in movements {
         let dir = movement.get_dir();
+        let next_pos = (pos.0 as i32 + dir.0, pos.1 as i32 + dir.1);
+
+        if is_off_grid(grid, next_pos) {
+            continue;
+        }
+
+        let ch = grid[next_pos.1 as usize][next_pos.0 as usize];
+        
+        match ch {
+            '.' => {
+                grid[pos.1][pos.0] = '.';
+                pos = (next_pos.0 as usize, next_pos.1 as usize);
+                grid[pos.1][pos.0] = '@';
+            },
+            '#' => {},
+            'O' => {
+                // Find the last box pos, or dont move at all.
+                let mut last_box_pos = (next_pos.0 + dir.0, next_pos.1 + dir.1);
+                let mut can_move = true;
+
+                loop {
+                    let cur_ch = grid[last_box_pos.1 as usize][last_box_pos.0 as usize]; 
+
+                    match cur_ch {
+                        '.' => break,
+                        '#' => {
+                            can_move = false;
+                            break;
+                        },
+                        _ => {}
+                    }
+
+                    last_box_pos = (last_box_pos.0 + dir.0, last_box_pos.1 + dir.1);
+                }
+
+                if can_move {
+                    grid[pos.1][pos.0] = '.';
+                    pos = (next_pos.0 as usize, next_pos.1 as usize);
+                    grid[pos.1][pos.0] = '@';
+                    grid[last_box_pos.1 as usize][last_box_pos.0 as usize] = 'O'
+                }
+            },
+            _ => {} 
+        }
     }
 
-    return Ok(10)
+    let mut total = 0;
+
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            let ch = grid[y][x];
+
+            if ch == 'O' {
+                let gps = y * 100 + x;
+                total += gps;
+            }
+        }
+    }
+
+    return total;
 }
 
 fn main() {
@@ -63,6 +123,6 @@ fn main() {
     let split: Vec<&str> = file_contents.split("\n\n").collect();
     let mut grid = parse_grid(split[0]);
     let movements = parse_movements(split[1]);
-    let result = simulate_robot(&mut grid, &movements).unwrap();
+    let result = simulate_robot(&mut grid, &movements);
     println!("{}", result);
 }
