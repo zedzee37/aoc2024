a8ccb21ccb9b0cc6a7ac8eb2dcf060715cb7f91d
zedzee <carsonuderitz@gmail.com>
2024-12-17 07:51:11 -0500

    remove that

--- day16/target/debug/deps/day16-c3589cccf15d13b1.d
+++
@@ -1,5 +0,0 @@
-/home/zedzee/aoc2024/day16/target/debug/deps/libday16-c3589cccf15d13b1.rmeta: src/main.rs
-
-/home/zedzee/aoc2024/day16/target/debug/deps/day16-c3589cccf15d13b1.d: src/main.rs
-
-src/main.rs:
