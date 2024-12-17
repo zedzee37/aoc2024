0d04469b8c3e6693fffd8f512e62ab0c9b2d2506
zedzee <carsonuderitz@gmail.com>
2024-12-17 07:50:56 -0500

    day 16 init

---
+++ day16/target/debug/deps/day16-1ea5614ff72b87c0.d
@@ -0,0 +1,5 @@
+/home/zedzee/aoc2024/day16/target/debug/deps/libday16-1ea5614ff72b87c0.rmeta: src/main.rs
+
+/home/zedzee/aoc2024/day16/target/debug/deps/day16-1ea5614ff72b87c0.d: src/main.rs
+
+src/main.rs:
---
+++ day16/target/debug/deps/day16-c3589cccf15d13b1.d
@@ -0,0 +1,5 @@
+/home/zedzee/aoc2024/day16/target/debug/deps/libday16-c3589cccf15d13b1.rmeta: src/main.rs
+
+/home/zedzee/aoc2024/day16/target/debug/deps/day16-c3589cccf15d13b1.d: src/main.rs
+
+src/main.rs:
