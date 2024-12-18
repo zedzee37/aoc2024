72a55f6ab4738d063847e35d0b90bb98b4fa86d1
zedzee <carsonuderitz@gmail.com>
2024-12-18 10:40:09 -0500

    astar implemented

--- day16/main.go
+++ day16/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -27,7 +29,206 @@ func (v Vec2) neg() Vec2 {
 	return v.mul(-1)
 }
 
-func parseInput(input string) []string {
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func (v Vec2) manhattanDst(other Vec2) int {
+	return abs(v[0] - other[0]) + abs(v[1] - other[1])
+}
+
+var cardinals = [4]Vec2{
+	{1, 0},
+	{-1, 0},
+	{0, 1},
+	{0, -1},
+}
+
+var emptyVec = Vec2{}
+
+type AStarNode struct {
+	pos Vec2
+	g int
+	f int
+	from *AStarNode
+}
+
+func (node AStarNode) getPathCost() int {
+	cost := 1
+	current := node.from
+	prevDir := emptyVec
+
+	for current != nil {
+		next := current.from
+	
+		if next == nil {
+			break
+		}
+
+		dir := next.pos.sub(current.pos)
+
+		cost++
+		current = next
+		if prevDir == emptyVec {
+			prevDir = dir
+		} else if prevDir != dir {
+			cost += 1000
+		}
+	}
+
+	return cost
+}
+
+func (node AStarNode) getPath() map[Vec2]bool {
+	path := make(map[Vec2]bool)
+	current := &node
+
+	for current != nil {
+		path[current.pos] = true
+		current = current.from
+	}
+
+	return path
+}
+
+type Grid []string
+
+func (grid Grid) printPath(path AStarNode) {
+	a := path.getPath()	
+
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid); x++ {
+			pos := Vec2{x, y}
+			
+			ch := grid.get(pos)
+
+			if a[pos] {
+				fmt.Print("P")
+			} else {
+				fmt.Print(string(ch))
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func (grid Grid) get(pos Vec2) byte {
+	return grid[pos[1]][pos[0]]
+}
+
+func (grid Grid) isOffGrid(pos Vec2) bool {
+	return pos[0] < 0 || pos[1] < 0 || pos[0] >= len(grid) || pos[1] >= len(grid)
+}
+
+func (grid Grid) getSurrounding(from Vec2) []Vec2 {
+	surrounding := make([]Vec2, 0)
+
+	for _, dir := range cardinals {
+		pos := from.add(dir)
+		
+		if !grid.isOffGrid(pos) && grid.get(pos) != '#' {
+			surrounding = append(surrounding, pos)	
+		}
+	}
+
+	return surrounding
+}
+
+func (grid Grid) findEndPoints() (Vec2, Vec2, error) {
+	startPoint := emptyVec
+	endPoint := emptyVec
+
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid); x++ {
+			if startPoint != emptyVec && endPoint != emptyVec {
+				return startPoint, endPoint, nil
+			}
+
+			pos := Vec2{x, y}
+			ch := grid.get(pos)
+			switch ch {
+			case 'S':
+				startPoint = pos
+			case 'E':
+				endPoint = pos
+			}
+		}
+	}
+
+	return startPoint, endPoint, fmt.Errorf("Failed to find start/end point of the path!")
+}
+
+func findLowestCost(nodes []AStarNode) int {
+	lowestCost := nodes[0].f
+	lowestCostIdx := 0
+	for i, node := range nodes {
+		if node.f < lowestCost {
+			lowestCostIdx = i
+			lowestCost = node.f
+		}
+	}
+	return lowestCostIdx
+}
+
+func (grid Grid) findShortestPath() (AStarNode, error) {
+	startPos, endPos, err := grid.findEndPoints()
+	
+	if err != nil {
+		return AStarNode{}, err
+	}
+
+	visited := make(map[Vec2]bool)
+	
+	current := make([]AStarNode, 0)
+	startDst := startPos.manhattanDst(endPos)
+	current = append(current, AStarNode{
+		pos: startPos,
+		g: 0,
+		f: startDst,
+		from: nil,
+	})
+
+	for len(current) > 0 {
+		lowestCostIdx := findLowestCost(current)	
+		lowestCostCell := current[lowestCostIdx]
+		current = slices.Delete(current, lowestCostIdx, lowestCostIdx+1)
+
+		if visited[lowestCostCell.pos] {
+			continue
+		}
+		
+		visited[lowestCostCell.pos] = true
+		surrounding := grid.getSurrounding(lowestCostCell.pos)
+		
+		for _, neighbor := range surrounding {
+			if visited[neighbor] {
+				continue
+			}
+
+			gCost := lowestCostCell.g + 1
+			dst := neighbor.manhattanDst(endPos)
+			cell := AStarNode{
+				pos: neighbor,
+				g: gCost,
+				f: dst + gCost,
+				from: &lowestCostCell,
+			}
+
+			if neighbor == endPos {
+				return cell, nil
+			}
+
+			current = append(current, cell)
+		}
+	}
+
+	return AStarNode{}, fmt.Errorf("Could not find viable path to end!")
+}
+
+func parseInput(input string) Grid {
 	return strings.Split(input, "\n")
 }
 
@@ -42,5 +243,12 @@ func main() {
 	check(err)
 
 	grid := parseInput(string(fileContents))
+	grid = grid[:len(grid)-1]
+	shortestPath, err := grid.findShortestPath()
+	check(err)
 
+	grid.printPath(shortestPath)
+
+	cost := shortestPath.getPathCost()
+	fmt.Println(cost)
 }
