f98ea6e6793ed3f7940205855910b6bf8a8955ac
zedzee <carsonuderitz@gmail.com>
2024-12-18 23:45:21 -0500

    GUHGHGHG

--- day16/main.py
+++ day16/main.py
@@ -36,16 +36,19 @@ def get_shortest_path(grid):
     if not res:
         return None
 
+    paths = []
     start_pos, end_pos = res
-    visited = set()
+    visited = {}
     current = []
     current.append({
         "pos": start_pos,
         "cost": 0,
-        "dir": 1 + 0j
+        "dir": 1 + 0j,
+        "length": 0,
+        "prev": None
     })
+    lowest_cost = 10e6
 
-    i = 0
     while len(current) > 0:
         min_cost_index = min(range(len(current)), key=lambda i: current[i]["cost"])
         node = current.pop(min_cost_index)
@@ -53,7 +56,7 @@ def get_shortest_path(grid):
         if node["pos"] in visited:
             continue
 
-        visited.add(node["pos"])
+        visited[node["pos"]] = node
         neighbor_directions = [
             node["dir"],
             right(node["dir"]),
@@ -70,23 +73,68 @@ def get_shortest_path(grid):
             if dir != node["dir"]:
                 cost += 1000
 
-
             neighbor_node = {
                 "pos": neighbor,
                 "dir": dir,
-                "cost": cost + node["cost"]
+                "cost": cost + node["cost"],
+                "prev": node,
             }
             
             if neighbor == end_pos:
-                return neighbor_node
-            
-            current.append(neighbor_node)
-        i += 1
+                if lowest_cost == None:
+                    lowest_cost = neighbor_node["cost"]
+                    paths.append(neighbor_node)
+                lowest_cost = min(lowest_cost, neighbor_node["cost"])
+                paths.append(neighbor_node)
 
+                new_paths = []
+                for path in paths:
+                    if path["cost"] <= lowest_cost:
+                        new_paths.append(path)
+                paths = new_paths
+            else:
+                current.append(neighbor_node)
+    return paths
+
+def get_path(end):
+    current = end
+    path = []
+
+    while current != None:
+        path.append(current)
+        current = current["prev"]
+
+        if current == None:
+            return path
+
+    return path
+
+def print_path(grid, path):
+    for y in range(len(grid)):
+        for x in range(len(grid)):
+            pos = x + (y * 1j)
+
+            if pos in path:
+                print("X", end="")
+            else:
+                print(grid[y][x], end="")
+        print()
+    print()
 
 with open("input.txt", 'r') as file:
     grid = file.read().split('\n')
     grid = grid[:len(grid) - 1]
 
-shortest_path = get_shortest_path(grid)
-print(shortest_path["cost"])
+visited = set()
+shortest_paths = get_shortest_path(grid)
+if shortest_paths:
+    for end in shortest_paths:
+        print(end)
+        print(end["cost"])
+        path = get_path(end)
+        print_path(grid, path)
+        
+        for pos in path:
+            visited.add(pos)
+    print(len(visited))
+
