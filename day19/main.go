225e914e346598132686c1d370ddaa08002e9591
zedzee37 <carsonuderitz@gmail.com>
2025-10-02 21:20:49 -0400

    weird tree

--- day19/main.go
+++ day19/main.go
@@ -5,6 +5,56 @@ import (
 	"os"
 )
 
+type PatternNode struct {
+	Leaves   []string
+	Children map[byte]*PatternNode
+	Parent   *PatternNode
+}
+
+func NewPatternNode() *PatternNode {
+	patternNode := new(PatternNode)
+
+	patternNode.Children = make(map[byte]*PatternNode, 0)
+	patternNode.Leaves = make([]string, 0)
+	patternNode.Parent = nil
+
+	return patternNode
+}
+
+func (patternNode *PatternNode) InsertPattern(pattern string) {
+	patternNode.insertPattern(pattern, 0)
+}
+
+func (patternNode *PatternNode) insertPattern(pattern string, depth int) {
+	if len(patternNode.Leaves) == 0 && len(patternNode.Children) == 0 {
+		patternNode.Leaves = append(patternNode.Leaves, pattern)
+		return
+	}
+
+	if len(patternNode.Children) != 0 {
+		for key, node := range patternNode.Children {
+			if pattern[depth] == key {
+				node.insertPattern(pattern, depth+1)
+				return
+			}
+		}
+	}
+
+	for i, leaf := range patternNode.Leaves {
+		if leaf[depth] == pattern[depth] {
+			patternNode.Leaves = append(patternNode.Leaves[0:i], patternNode.Leaves[i+1:len(patternNode.Leaves)]...)
+
+			newNode := NewPatternNode()
+			newNode.Leaves = append(newNode.Leaves, leaf, pattern)
+			patternNode.Children[leaf[depth]] = newNode
+
+			return
+		}
+	}
+
+	patternNode.Leaves = append(patternNode.Leaves, pattern)
+}
+
 // func isPatternPossible(availablePatterns []string, pattern string) bool {
 
 // }
@@ -56,19 +106,33 @@ func parseInput(fp string) ([]string, []string, error) {
 	return availablePatterns, targetPatterns, nil
 }
 
-func main() {
-	_, targetPatterns, err := parseInput("input.txt")
-
-	if err != nil {
-		panic(err.Error())
+func printPatternNode(patternNode *PatternNode) {
+	println("Leaves: ")
+	for _, leaf := range patternNode.Leaves {
+		println(leaf)
 	}
+	println()
 
-	// for _, pattern := range availablePatterns {
-	// 	fmt.Println(pattern)
+	for key, child := range patternNode.Children {
+		fmt.Println("Printing leaf with key: {} --", string(key))
+		println()
+		printPatternNode(child)
+	}
+}
+
+func main() {
+	// _, _, err := parseInput("input.txt")
+
+	// if err != nil {
+	// 	panic(err.Error())
 	// }
 
-	fmt.Println()
-	for _, pattern := range targetPatterns {
-		fmt.Println(pattern)
-	}
+	headNode := NewPatternNode()
+	headNode.InsertPattern("gug")
+	headNode.InsertPattern("gork")
+	headNode.InsertPattern("gork")
+	headNode.InsertPattern("gag")
+	headNode.InsertPattern("arg")
+
+	printPatternNode(headNode)
 }
